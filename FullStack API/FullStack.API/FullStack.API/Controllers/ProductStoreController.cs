e88a9c0e691fd46b4b4b95e53a0e5b8d6b49c665
sveezze1 <sveezze1@gmail.com>
2024-02-18 22:39:57 +0300

    ready

--- FullStack API/FullStack.API/FullStack.API/Controllers/ProductStoreController.cs
+++ FullStack API/FullStack.API/FullStack.API/Controllers/ProductStoreController.cs
@@ -1,7 +1,8 @@
-﻿using FullStack.API.Data;
-using FullStack.API.models;
+﻿using FullStack.API.models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace FullStack.API.Controllers
 {
@@ -9,22 +10,40 @@ namespace FullStack.API.Controllers
     [Route("api/[controller]")]
     public class ProductStoreController : Controller
     {
-        private readonly FullStackDbContext db;
+        private readonly IConfiguration _configuration;
 
-        public ProductStoreController(FullStackDbContext fullStackDbContext)
+        public ProductStoreController(IConfiguration configuration)
         {
-            db = fullStackDbContext;
+            _configuration = configuration;
         }
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProducts()
         {
-            var products = await db.Products.FromSqlRaw("SELECT TOP (1000) [id1],[title]," +
-                "[category]," +
-                "[price]," +
-                "[UOM]" +
-                "FROM [mireaDbTask].[dbo].[Products]").ToListAsync();
+            string connectionString = _configuration.GetConnectionString("FullStackConnectionString");
+            string query = "SELECT P.title, P.price, P.UOM, WO.priceForSale FROM Products P JOIN WOProducts WO ON P.id1 = WO.id1";
+            var result = new DataTable();
 
-            return Ok(products);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.CommandTimeout = 3000;
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+
+                    da.Fill(result);
+                }
+                connection.Close();
+            }
+            var data = result.AsEnumerable().Select(x => new Products()
+            {
+                title = x["title"].ToString(),
+                price = x["price"].ToString(),
+                UOM = x["UOM"].ToString(),
+                priceForSale = x["priceForSale"].ToString()
+            }).ToList();
+            return Json(data);
         }
     }
 }

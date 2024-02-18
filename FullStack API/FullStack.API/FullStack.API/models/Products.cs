460f0b44103d94da4ba1e003383a278e336140c3
sveezze1 <sveezze1@gmail.com>
2024-02-18 22:30:58 +0300

    fixes for back

--- FullStack API/FullStack.API/FullStack.API/models/Products.cs
+++ FullStack API/FullStack.API/FullStack.API/models/Products.cs
@@ -4,10 +4,8 @@ namespace FullStack.API.models;
 
 public class Products
 {
-    [Key]
-	public int id1 { get; set; }
 	public string title { get; set; }
-    public string category { get; set; }
-    public int price { get; set; }
+	public string price { get; set; }
     public string UOM { get; set; }
+    public string priceForSale { get; set; }
 }

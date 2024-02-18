808b2a385a8a124ae2388c26c6278ee73e2e65cb
sveezze1 <sveezze1@gmail.com>
2024-02-18 19:24:49 +0300

    next fixes

--- FullStack UI/FullStack.UI/src/app/app.module.ts
+++ FullStack UI/FullStack.UI/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import {DxSelectBoxModule} from "devextreme-angular";
 import { UserComponentComponent } from './components/user-component/user-component.component';
 import { ProductsComponent } from './components/products-component/products.component';
 import { ProductsCountComponent } from './components/products-count/products-count.component';
@@ -14,6 +13,7 @@ import { SalesComponentComponent } from './components/sales-component/sales-comp
 import { SuppliersComponentComponent } from './components/suppliers-component/suppliers-component.component';
 import { WarehouseComponentComponent } from './components/warehouse-component/warehouse-component.component';
 import { ReviewComponentComponent } from './components/review-component/review-component.component';
+import { DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
@@ -24,14 +24,15 @@ import { ReviewComponentComponent } from './components/review-component/review-c
     SalesComponentComponent,
     SuppliersComponentComponent,
     WarehouseComponentComponent,
-    ReviewComponentComponent
+    ReviewComponentComponent,
+    ProductsComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
         FormsModule,
-        DxSelectBoxModule
+        DxDataGridModule
     ],
   providers: [],
   bootstrap: [AppComponent]

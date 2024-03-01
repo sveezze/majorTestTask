9d6b16b5cee4c71c62476846ed8e83f0e19a124b
sveezze1 <sveezze1@gmail.com>
2024-03-01 17:23:36 +0300

    fix

--- FullStack UI/FullStack.UI/src/app/services/productstore.service.ts
+++ FullStack UI/FullStack.UI/src/app/services/productstore.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
-import { ProductsToBuyModel } from '../model/ProductsToBuyModel';
+import { ProductsToBuyModel } from '../model/productsToBuyModel';
 
 @Injectable({
   providedIn: 'root'
